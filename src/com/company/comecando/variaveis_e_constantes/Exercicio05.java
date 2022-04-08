b05a62fa13f279541d06ee619e8cbda1992e9528
filipealvesvaz <filipealvesvaz@gmail.com>
2022-04-08 16:04:10 -0300

    initial

---
+++ src/com/company/comecando/variaveis_e_constantes/Exercicio05.java
@@ -0,0 +1,20 @@
+package com.company.comecando.variaveis_e_constantes;
+
+//transforme o valor 70 em uma constante
+
+import java.util.Scanner;
+
+public class Exercicio05 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite a sua nota: ");
+        Double notaDoAluno = scanner.nextDouble();
+        Boolean passouDeAno = notaDoAluno >= 70;
+        if (passouDeAno) {
+            System.out.println("Parabéns! Você passou de ano.");
+        } else {
+            System.out.println("Infelizmente, ficou de recuperação.");
+        }
+        scanner.close();
+    }
+}
