902eb763d79a85b3bf371751fe42d0f490d0ad3b
filipealvesvaz <filipealvesvaz@gmail.com>
2022-04-11 16:12:52 -0300

    initial

---
+++ src/com/company/comecando/operadores/Exercicio03.java
@@ -0,0 +1,20 @@
+package com.company.comecando.operadores;
+
+/*
+
+Faça um programa que receba:
+
+O valor da conta de luz
+Conta de água
+Conta de telefone
+Escola do filho
+Fatura do cartão
+Gastos com supermercado
+... e mostre o gasto total que a família teve no mês. Não esqueça de usar muito o operador "+=".
+
+ */
+
+
+
+public class Exercicio03 {
+}
