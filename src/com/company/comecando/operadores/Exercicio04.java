5b2969883e1a19ce664f38d13dbbff63a2f2c75b
filipealvesvaz <filipealvesvaz@gmail.com>
2022-04-13 14:05:13 -0300

    initial

--- src/com/company/comecando/operadores/Exercicio04.java
+++ src/com/company/comecando/operadores/Exercicio04.java
@@ -9,5 +9,25 @@ Nas regras desse programa a pessoa vai precisar ter, pelo menos, 55 anos e ter c
  Se ela passar nessas duas condições, então mostre pra ela uma mensagem dizendo que ela pode aposentar.
  */
 
+import java.util.Scanner;
+
 public class Exercicio04 {
+    static final Integer IDADE_MINIMA_PARA_APOSENTAR = 55;
+    static final Integer TEMPO_MINIMO_DE_CONTRIBUICAO = 25;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Idade: ");
+        Integer idade = scanner.nextInt();
+        System.out.print("Tempo de contribuição: ");
+        Integer contribuicao = scanner.nextInt();
+        Boolean temIdadeParaAposentar = idade >= IDADE_MINIMA_PARA_APOSENTAR;
+        Boolean temTempoDeContribuicaoSuficiente = contribuicao >= TEMPO_MINIMO_DE_CONTRIBUICAO;
+        Boolean podeAposentar = temIdadeParaAposentar && temTempoDeContribuicaoSuficiente;
+        if (podeAposentar) {
+            System.out.println("Sim! Pode aposentar.");
+        } else {
+            System.out.println("Não vai ser possível aposentar agora.");
+        }
+        scanner.close();
+    }
 }
