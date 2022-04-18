bac96d5b11b07ba68df43f3fe6a66220a8c60a46
filipealvesvaz <filipealvesvaz@gmail.com>
2022-04-18 14:48:13 -0300

    initial

---
+++ src/com/company/comecando/estruturas_de_decisao/Exercicio03.java
@@ -0,0 +1,12 @@
+package com.company.comecando.estruturas_de_decisao;
+
+/*
+
+Crie um programa que vai receber um número que será referente ao dia da semana (de 1 até 7). Dependendo do número
+informado você deve imprimir o nome desse dia.
+Se o número 1 for informado, então deverá ser impresso "domingo", se 2, então "segunda-feira" e por ai vai.
+
+ */
+
+public class Exercicio03 {
+}
