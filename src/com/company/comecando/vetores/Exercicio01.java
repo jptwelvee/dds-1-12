747f5df019b0e48a5b19cf587a2e4266689ffa82
filipealvesvaz <filipealvesvaz@gmail.com>
2022-04-20 16:35:15 -0300

    initial

--- src/com/company/comecando/vetores/Exercicio01.java
+++ src/com/company/comecando/vetores/Exercicio01.java
@@ -1,11 +1,18 @@
 package com.company.comecando.vetores;
 
-/*
-
-Crie um sistema onde o usuário vai cadastrar, utilizando o console, as 5 tarefas mais importantes do dia dele.
-Armazene cada uma dessas tarefas em um vetor e, no final, imprima as tarefas novamente.
-
- */
-
+import java.util.Scanner;
 public class Exercicio01 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] tarefas = new String[5];
+        for(int i = 0; i < tarefas.length; i++) {
+            System.out.print("Tarefa " + i + ": ");
+            tarefas[i] = scanner.nextLine();
+        }
+        System.out.println("Suas tarefas são: ");
+        for(int i = 0; i < tarefas.length; i++) {
+            System.out.println(i + ": " + tarefas[i]);
+        }
+        scanner.close();
+    }
 }
