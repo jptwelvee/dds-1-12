d8a75fc76e5b0d5b8b73bfd4ee1e6f95f0c59e83
filipealvesvaz <filipealvesvaz@gmail.com>
2022-04-29 14:32:31 -0300

    Initial

--- src/com/company/comecando/metodos/Exercicio02.java
+++ src/com/company/comecando/metodos/Exercicio02.java
@@ -2,7 +2,7 @@ package com.company.comecando.metodos;
 
 /*
 
-Identifique um novo padrão no algoritmo da aula passada
+Identifique um novo padrão no algoritmo "RecebendoParametros"
 
 Estude um pouco mais o algoritmo anterior, e identifique algum trecho dele que pode ser separado para colocar
 em um novo método criado por você.
