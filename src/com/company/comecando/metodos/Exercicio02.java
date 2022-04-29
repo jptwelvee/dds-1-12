dd274f02879279ccaff86eb0cfc0995671cef18f
filipealvesvaz <filipealvesvaz@gmail.com>
2022-04-29 14:29:42 -0300

    Initial

---
+++ src/com/company/comecando/metodos/Exercicio02.java
@@ -0,0 +1,13 @@
+package com.company.comecando.metodos;
+
+/*
+
+Identifique um novo padrão no algoritmo da aula passada
+
+Estude um pouco mais o algoritmo anterior, e identifique algum trecho dele que pode ser separado para colocar
+em um novo método criado por você.
+
+ */
+
+public class Exercicio02 {
+}
