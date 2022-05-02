f928f03dd09e1611f0a24289bb1c8cab19ca4e02
filipealvesvaz <filipealvesvaz@gmail.com>
2022-05-02 14:39:20 -0300

    Initial

--- src/com/company/comecando/metodos/Recursividade.java
+++ src/com/company/comecando/metodos/Recursividade.java
@@ -4,8 +4,18 @@ public class Recursividade {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         imprimirTraco();
+        String[] cursos = new String[] {"Java EE", "Spring", "Java OO Avançado"};
         imprimir("Escolha dentre os cursos abaixo: ");
         IterarEExibirPosicoesDoVetorString(cursos);
+        System.out.print("O curso que você escolheu é o: ");
+        Integer posicaoCursoEscolhido = scanner.nextInt();
+        Boolean posicaoValida
+    }
+
+    private static void IterarEExibirPosicoesDoVetorString(String[] vetor) {
+        for (int i = 0 ; i < vetor.length; i++) {
+            imprimir("[" +i+ "]" + vetor[i]);
+        }
     }
 
     private static void imprimirTraco() {
