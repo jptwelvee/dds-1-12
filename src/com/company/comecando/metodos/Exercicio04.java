472e42158c2f5ed81df10a2bba7021e847c02da5
filipealvesvaz <filipealvesvaz@gmail.com>
2022-05-02 17:19:03 -0300

    Initial

--- src/com/company/comecando/metodos/Exercicio04.java
+++ src/com/company/comecando/metodos/Exercicio04.java
@@ -32,5 +32,20 @@ incrementá-lo para as próximas chamadas que forem feitas.
 
  */
 
+import java.util.Scanner;
 public class Exercicio04 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Informe o número: ");
+        Integer numero = scanner.nextInt();
+        System.out.println("----------------------------------");
+        imprimirTabuada(numero, 0);
+        scanner.close();
+    }
+    static void imprimirTabuada(Integer multiplicando, Integer multiplicador) {
+        System.out.println(multiplicando + " x " + multiplicador + " = " + (multiplicando * multiplicador));
+        if (++multiplicador <= 10) {
+            imprimirTabuada(multiplicando, multiplicador);
+        }
+    }
 }
