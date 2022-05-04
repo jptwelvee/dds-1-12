ecbd0e9f54f0720c2812fd4f6d2c09b0361275e3
filipealvesvaz <filipealvesvaz@gmail.com>
2022-05-04 14:11:06 -0300

    Initial

--- src/com/company/programacao_orientada_a_objetos/PrimeiraClasse.java
+++ src/com/company/programacao_orientada_a_objetos/PrimeiraClasse.java
@@ -1,4 +1,17 @@
 package com.company.programacao_orientada_a_objetos;
 
 public class PrimeiraClasse {
+
+    public static void main(String[] args) {
+        Produto produto = new Produto();
+        produto.nome = "Smartphone";
+        produto.precoUnitario = 500.0;
+        produto.quantidade = 15;
+
+        exibirQuantidadeEmEstoque(produto);
+    }
+
+    private static void exibirQuantidadeEmEstoque(Produto produto) {
+        System.out.println("Quantidade em estoque do produto " + produto.nome + "é de " + );
+    }
 }
