a2a06f169a833b77fe4f8524e35fb9293670e428
filipealvesvaz <filipealvesvaz@gmail.com>
2022-05-04 13:56:41 -0300

    Initial

---
+++ src/com/company/programacao_orientada_a_objetos/PrimeiraClasse.java
@@ -0,0 +1,4 @@
+package com.company.programacao_orientada_a_objetos;
+
+public class PrimeiraClasse {
+}
---
+++ src/com/company/variaveis_e_constantes/Exercicio05.java
@@ -0,0 +1,22 @@
+package com.company.variaveis_e_constantes;
+
+//transforme o valor 70 em uma constante
+
+import java.util.Scanner;
+
+public class Exercicio05 {
+    private static final Integer NOTA_MINIMA_PARA_PASSAR_DE_ANO = 70;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite a sua nota: ");
+        Double notaDoAluno = scanner.nextDouble();
+        Boolean passouDeAno = notaDoAluno >= NOTA_MINIMA_PARA_PASSAR_DE_ANO;
+        if (passouDeAno) {
+            System.out.println("Parabéns! Você passou de ano.");
+        } else {
+            System.out.println("Infelizmente, ficou de recuperação.");
+        }
+        scanner.close();
+    }
+}
