b625583adca96e5f101ca28069b29127c56b37d2
filipealvesvaz <filipealvesvaz@gmail.com>
2022-05-09 16:50:12 -0300

    initial

--- src/com/company/leitura_e_escrita_de_dados_em_arquivos/exercicio01/Exercicio01.java
+++ src/com/company/leitura_e_escrita_de_dados_em_arquivos/exercicio01/Exercicio01.java
@@ -1,4 +1,14 @@
 package com.company.leitura_e_escrita_de_dados_em_arquivos.exercicio01;
+/*
+Crie uma lista de tarefas e salve a lista em um arquivo
+
+Crie um programa que vai receber, através do console, uma lista de atividades que vai ajudar o usuário a planejar suas tarefas para o dia seguinte.
+
+Depois que o usuário informar, uma a uma, suas tarefas, as mesmas serão salvas em um arquivo de texto - cada tarefa vai ocupar uma linha desse arquivo.
+
+Para receber cada tarefa do usuário, use o laço while. Enquanto o usuário não digitar um "x" no console, continue pedindo que ele informe as atividades.
+
+ */
 
 public class Exercicio01 {
 }
