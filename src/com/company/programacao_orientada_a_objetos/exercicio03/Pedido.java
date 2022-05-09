298dbe4745227df4edf4999704106d7baafed3d9
filipealvesvaz <filipealvesvaz@gmail.com>
2022-05-09 16:48:17 -0300

    Initial

--- src/com/company/programacao_orientada_a_objetos/exercicio03/Pedido.java
+++ src/com/company/programacao_orientada_a_objetos/exercicio03/Pedido.java
@@ -1,31 +1,41 @@
 package com.company.programacao_orientada_a_objetos.exercicio03;
+
 public class Pedido {
     Integer codigo;
     Double subtotal;
     Double desconto;
-    Double getTotal() {
-        return subtotal - desconto;
-    }
-    void setTotal(Double total) {
-        // Não é mais necessário
-    }
-    Integer getCodigo() {
+    Double total;
+
+    public Integer getCodigo() {
         return codigo;
     }
-    void setCodigo(Integer codigo) {
+
+    public void setCodigo(Integer codigo) {
         this.codigo = codigo;
     }
-    Double getSubtotal() {
+
+    public Double getSubtotal() {
         return subtotal;
 
     }
-    void setSubtotal(Double subtotal) {
+
+    public void setSubtotal(Double subtotal) {
         this.subtotal = subtotal;
     }
-    Double getDesconto() {
+
+    public Double getDesconto() {
         return desconto;
     }
-    void setDesconto(Double desconto) {
+
+    public void setDesconto(Double desconto) {
         this.desconto = desconto;
+    }
+
+    public Double getTotal() {
+        return subtotal - desconto;
+    }
+
+    public void setTotal(Double total) {
+        this.total = total;
     }
 }
