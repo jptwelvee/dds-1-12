2cd826f7ba88315b6f71c1f0ad4be00b35e76de5
filipealvesvaz <filipealvesvaz@gmail.com>
2022-05-11 16:47:53 -0300

    Exercicio01.java

--- src/com/company/orientacao_a_objetos/composicao_de_objetos/Principal.java
+++ src/com/company/orientacao_a_objetos/composicao_de_objetos/Principal.java
@@ -1,4 +1,17 @@
 package com.company.orientacao_a_objetos.composicao_de_objetos;
 
 public class Principal {
+
+    public static void main(String[] args) {
+        Carro meuCarro = new Carro();
+        meuCarro.anoDeFabricacao = 2011;
+        meuCarro.modelo = "Palio";
+        meuCarro.cor = "Prata";
+        meuCarro.fabricante = "Fiat";
+
+        meuCarro.dono = new Proprietario();
+        meuCarro.dono.nome = "João";
+        meuCarro.dono.bairro = "Centro";
+    }
+
 }
