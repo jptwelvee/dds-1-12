974a0a9bb5347ba0cbe29efeec78f51e1d32c9eb
filipealvesvaz <filipealvesvaz@gmail.com>
2022-05-16 14:45:13 -0300

    Exercicio01.java

---
+++ src/com/company/orientacao_a_objetos/exercicio03/Estoque.java
@@ -0,0 +1,12 @@
+package com.company.orientacao_a_objetos.exercicio03;
+public class Estoque {
+    Produto[] produtos;
+    void listarProdutos(){
+        System.out.println("\nProdutos em estoque");
+        System.out.println("---------------------");
+        for (int i = 0; i < produtos.length; i++){
+            produtos[i].descrever();
+        }
+    }
+
+}
---
+++ src/com/company/orientacao_a_objetos/exercicio03/Produto.java
@@ -0,0 +1,8 @@
+package com.company.orientacao_a_objetos.exercicio03;
+public class Produto {
+    String descricao;
+    int quantidade;
+    void descrever() {
+        System.out.println(descricao + " - " + quantidade + " itens");
+    }
+}
