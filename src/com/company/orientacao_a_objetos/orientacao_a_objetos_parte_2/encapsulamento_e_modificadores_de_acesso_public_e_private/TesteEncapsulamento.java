05fbabe7ccd2d72c9d7763ee7f7a4e9c35c5cbdb
filipealvesvaz <filipealvesvaz@gmail.com>
2022-05-16 16:38:56 -0300

    this, construtores

--- src/com/company/orientacao_a_objetos/orientacao_a_objetos_parte_2/encapsulamento_e_modificadores_de_acesso_public_e_private/TesteEncapsulamento.java
+++ src/com/company/orientacao_a_objetos/orientacao_a_objetos_parte_2/encapsulamento_e_modificadores_de_acesso_public_e_private/TesteEncapsulamento.java
@@ -1,4 +1,10 @@
 package com.company.orientacao_a_objetos.orientacao_a_objetos_parte_2.encapsulamento_e_modificadores_de_acesso_public_e_private;
-
 public class TesteEncapsulamento {
+    public static void main(String[] args) {
+        ArCondicionado ar = new ArCondicionado();
+        ar.trocarTemperatura(21);
+        System.out.println("Temperatura do ar: " + ar.obterTemperatura() + "°");
+        ar.trocarTemperatura(10);
+        System.out.println("Temperatura do ar: " + ar.obterTemperatura() + "°");
+    }
 }
