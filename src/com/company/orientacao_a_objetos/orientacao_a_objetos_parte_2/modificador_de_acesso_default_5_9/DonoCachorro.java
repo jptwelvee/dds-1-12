eccd31573959a6b32e8261c04420e416da09c735
filipealvesvaz <filipealvesvaz@gmail.com>
2022-05-18 15:01:47 -0300

    this, construtores

---
+++ src/com/company/orientacao_a_objetos/orientacao_a_objetos_parte_2/modificador_de_acesso_default_5_9/DonoCachorro.java
@@ -0,0 +1,6 @@
+package com.company.orientacao_a_objetos.orientacao_a_objetos_parte_2.modificador_de_acesso_default_5_9;
+public class DonoCachorro {
+    void ensinarCachorroSentar(Cachorro cachorro){
+        cachorro.sentar();
+    }
+}
