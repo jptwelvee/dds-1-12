48079d3aa6855b965561d8a6f80bef0e5d6e8169
filipealvesvaz <filipealvesvaz@gmail.com>
2022-05-18 14:33:20 -0300

    this, construtores

---
+++ src/com/company/orientacao_a_objetos/orientacao_a_objetos_parte_2/desafio_objeto_this_e_construtores/Fornecedor.java
@@ -0,0 +1,10 @@
+package com.company.orientacao_a_objetos.orientacao_a_objetos_parte_2.desafio_objeto_this_e_construtores;
+public class Fornecedor {
+    String nome;
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+}
