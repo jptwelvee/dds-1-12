7616cd1919340cda5d2696705afffaee670c78ad
filipealvesvaz <filipealvesvaz@gmail.com>
2022-05-25 14:37:45 -0300

    heranca e modificador protected

--- src/com/company/orientacao_a_objetos/orientacao_a_objetos_parte_2/_5_14_heranca_e_modificador_protected/Pessoa.java
+++ src/com/company/orientacao_a_objetos/orientacao_a_objetos_parte_2/_5_14_heranca_e_modificador_protected/Pessoa.java
@@ -1,4 +1,10 @@
 package com.company.orientacao_a_objetos.orientacao_a_objetos_parte_2._5_14_heranca_e_modificador_protected;
 
 public class Pessoa {
+    String nome;
+    protected int idade;
+    public void seApresentar(){
+        System.out.println("Olá, eu sou " + nome +
+                ", e tenho " + idade + "anos.");
+    }
 }
