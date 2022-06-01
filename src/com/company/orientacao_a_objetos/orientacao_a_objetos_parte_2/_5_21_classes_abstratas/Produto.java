79541511f4334c114695be3e72f2f0c05e04f817
filipealvesvaz <filipealvesvaz@gmail.com>
2022-06-01 15:47:47 -0300

    desafio polimorfismo e classes abstratas

---
+++ src/com/company/orientacao_a_objetos/orientacao_a_objetos_parte_2/_5_21_classes_abstratas/Produto.java
@@ -0,0 +1,8 @@
+package com.company.orientacao_a_objetos.orientacao_a_objetos_parte_2._5_21_classes_abstratas;
+public abstract class Produto {
+    String descricao;
+    public abstract void imprimirDescricao();
+    public String getDescricao() {
+        return descricao;
+    }
+}
---
+++ src/com/company/orientacao_a_objetos/orientacao_a_objetos_parte_2/_5_21_classes_abstratas/ProdutoPerecivel.java
@@ -0,0 +1,8 @@
+package com.company.orientacao_a_objetos.orientacao_a_objetos_parte_2._5_21_classes_abstratas;
+public class ProdutoPerecivel extends Produto{
+    String dataValidade;
+    @Override
+    public void imprimirDescricao() {
+        System.out.println("Descrição: " + super.getDescricao() + ". Vencendo em: " + dataValidade);
+    }
+}
