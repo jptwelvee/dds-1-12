d78690a7927126bb6e9b58b662eaf76b894078f2
filipealvesvaz <filipealvesvaz@gmail.com>
2022-06-01 14:11:08 -0300

    exercicio sobrecarga

--- src/com/company/orientacao_a_objetos/orientacao_a_objetos_parte_2/_5_19_exercicio_sobrecarga/TesteMaquinaCafe.java
+++ src/com/company/orientacao_a_objetos/orientacao_a_objetos_parte_2/_5_19_exercicio_sobrecarga/TesteMaquinaCafe.java
@@ -16,5 +16,11 @@ Dica do professor: use o debugger da IDE e execute o exemplo passo a passo. Isso
 
  */
 class TesteMaquinaCafe {
-
+    public static void main(String[] args) {
+        MaquinaCafe maquina = new MaquinaCafe();
+        maquina.acucarDisponivel = 30;
+        maquina.fazerCafe(10);
+        maquina.fazerCafe(15);
+        maquina.fazerCafe();
+    }
 }
